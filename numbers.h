5e4a6443de7f8752a2ffab0e95520f8be028fcb8
Duong Quoc Tue <32580806+rinzlerr@users.noreply.github.com>
2019-03-25 16:29:35 +0700

    init

---
+++ numbers.h
@@ -0,0 +1,8 @@
+class Text;
+
+class Numbers {
+
+public:
+
+ const int special[10] = { 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+};
---
+++ text.h
@@ -0,0 +1,25 @@
+class Numbers;
+
+class Text {
+
+public:
+
+ const char * units[10] = {"",
+						"one ", "two ", "three ",
+						"four ", "five ", "six ",
+						"seven ", "eight ", "nine " };
+ const char * tens[10] = { "",
+					   "ten ", "twenty ", "thirty ",
+					   "forty ", "fifty ", "sixty ",
+					   "seventy ", "eighty ", "ninety " };
+ const char * hundreds[10] = { "",
+							"one hundred ", "two hundreds ", "three hundreds ",
+							"four hundreds ", "five hundreds ", "six hundreds ",
+							"seven hundreds ", "eight hundreds ", "nine hundreds " };
+ const char * groups[10] = { "", "thousand ", "million ", "billion " };
+ const char * groups2[10] = { "", "thousands ", "millions ", "billions " };
+ const char * specialTxt[10] = { "eleven ", "twelve ", "thirteen ",
+							  "fourteen ", "fifteen ", "sixteen ",
+							  "seventeen ", "eighteen ", "nineteen " };
+ const char * andTxt = "and ";
+};
